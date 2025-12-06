ddf12389894f3ee462928ddaaf80b88b4c4b83ad
shaly-ct <57600351+shaly-ct@users.noreply.github.com>
2025-12-07 02:24:14 +0530

    Add hf_hub_download import to train.py

--- tourism_project/model_building/train.py
+++ tourism_project/model_building/train.py
@@ -14,6 +14,7 @@ import os
 # for hugging face space authentication to upload files
 from huggingface_hub import login, HfApi, create_repo
 from huggingface_hub.utils import RepositoryNotFoundError, HfHubHTTPError
+from huggingface_hub import hf_hub_download
 import mlflow
 
 mlflow.set_tracking_uri("http://localhost:5000")
